54a81e8c86ea0001df16be5681783aa59927736c
DmKeller <dmit.keller@gmail.com>
2023-06-13 23:40:29 +0300

    ColorizedApp completed

--- ColorizedAppSUI/ContentView.swift
+++ ColorizedAppSUI/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     @State private var redSliderValue = Double.random(in: 0...255)
     @State private var greenSliderValue = Double.random(in: 0...255)
     @State private var blueSliderValue = Double.random(in: 0...255)
-
+    
     var body: some View {
         ZStack {
             Color("backgroundBlue")
@@ -23,11 +23,11 @@ struct ContentView: View {
                     green: $greenSliderValue,
                     blue: $blueSliderValue
                 )
-
+                
                 ColorSliderView(value: $redSliderValue, color: .red)
                 ColorSliderView(value: $greenSliderValue, color: .green)
                 ColorSliderView(value: $blueSliderValue, color: .blue)
-
+                
                 Spacer()
             }
             .padding(.top, 20)
@@ -45,12 +45,12 @@ struct ColorView: View {
     @Binding var red: Double
     @Binding var green: Double
     @Binding var blue: Double
-
+    
     var body: some View {
         Rectangle()
             .fill(Color(red: red/255, green: green/255, blue: blue/255))
             .overlay(RoundedRectangle(cornerRadius: 20)
-            .stroke(Color.white, lineWidth: 2))
+                .stroke(Color.white, lineWidth: 2))
             .frame(width: 340, height: 170)
             .cornerRadius(20)
     }
@@ -59,21 +59,20 @@ struct ColorView: View {
 struct ColorSliderView: View {
     @Binding var value: Double
     let color: Color
-
+    
     var body: some View {
-            HStack {
-                Text("0")
-                    .foregroundColor(color)
-                    .fontWeight(.bold)
-
-                Slider(value: $value, in: 0...255, step: 1)
-                    .accentColor(color)
-                    .frame(width: 260)
-
-                TextField("", value: $value, formatter: NumberFormatter(), onCommit: { })
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .frame(width: 50)
-
-            }
+        HStack {
+            Text("0")
+                .foregroundColor(color)
+                .fontWeight(.bold)
+            
+            Slider(value: $value, in: 0...255, step: 1)
+                .accentColor(color)
+                .frame(width: 260)
+            
+            TextField("", value: $value, formatter: NumberFormatter(), onCommit: { })
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .frame(width: 50)
         }
     }
+}
