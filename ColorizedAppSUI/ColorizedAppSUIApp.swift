70d4734c65a99bbff7206ed1b796ed467d1186eb
DmKeller <dmit.keller@gmail.com>
2023-06-13 22:57:55 +0300

    Initial Commit

---
+++ ColorizedAppSUI/ColorizedAppSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedAppSUIApp.swift
+//  ColorizedAppSUI
+//
+//  Created by Келлер Дмитрий on 13.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedAppSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
